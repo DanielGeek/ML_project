c4c131f66868a893ef71119d94b6be38d9b32477
Daniel Ángel Barreto <salomons.cripto@gmail.com>
2026-09-14 15:24:24 -0400

    feat: add notebook examples and dataset

--- src/exception.py
+++ src/exception.py
@@ -2,6 +2,7 @@ import sys
 from src.logger import logging
 from types import ModuleType
 
+
 def error_message_detail(error, error_detail: ModuleType):
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename

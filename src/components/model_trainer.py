6d6e21754e470398840e94384a22d810bbe06f86
Daniel Ángel Barreto <salomons.cripto@gmail.com>
2026-09-23 13:32:00 -0400

    feat: add prediction

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -53,12 +53,12 @@ class ModelTrainer:
                     # 'splitter':['best','random'],
                     # 'max_features':['sqrt','log2'],
                 },
-                "Random Forest":{
+                "Random Forest": {
                     # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
                     # 'max_features':['sqrt','log2',None],
                     'n_estimators': [8, 16, 32, 64, 128, 256]
                 },
-                "Gradient Boosting":{
+                "Gradient Boosting": {
                     # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
                     'learning_rate':[.1, .01, .05, .001],
                     'subsample':[0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
@@ -66,17 +66,17 @@ class ModelTrainer:
                     # 'max_features':['auto','sqrt','log2'],
                     'n_estimators': [8, 16, 32, 64, 128, 256]
                 },
-                "Linear Regression":{},
-                "XGBRegressor":{
+                "Linear Regression": {},
+                "XGBRegressor": {
                     'learning_rate': [.1, .01, .05, .001],
                     'n_estimators': [8, 16, 32, 64, 128, 256]
                 },
-                "CatBoosting Regressor":{
+                "CatBoosting Regressor": {
                     'depth': [6, 8, 10],
                     'learning_rate': [0.01, 0.05, 0.1],
                     'iterations': [30, 50, 100]
                 },
-                "AdaBoost Regressor":{
+                "AdaBoost Regressor": {
                     'learning_rate':[.1, .01, 0.5, .001],
                     # 'loss':['linear','square','exponential'],
                     'n_estimators': [8, 16, 32, 64, 128, 256]
